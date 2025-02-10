a8b789cb486ff8cb6eaa905a5dc31395606d907b
Yaroslav Bryanskiy <yaroslav.bry@mail.ru>
2025-02-10 20:08:50 +0300

    added sh file

---
+++ init-zsh-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "$(date)"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+brew tap homebrew/cask-fonts
+brew install --cask font-meslo-lg-nerd-font
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
