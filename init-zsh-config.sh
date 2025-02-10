5ef7f500efb5339d5f5737969a9c7dc37c9bb966
Yaroslav Bryanskiy <yaroslav.bry@mail.ru>
2025-02-10 20:12:20 +0300

    changed sh file

--- init-zsh-config.sh
+++ init-zsh-config.sh
@@ -9,3 +9,6 @@ brew install --cask font-meslo-lg-nerd-font
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+ln -s ~/zsh-config/.zshrc ~/.zshrc
+ln -s ~/zsh-config/.p10k.zsh ~/.p10k.zsh
+
